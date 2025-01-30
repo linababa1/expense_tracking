b8db38bc613a7bf4ad7046180da2ef7a9fd86156
linababa1 <linababa987@gmail.com>
2025-01-30 12:10:56 +0200

    Update expense_item.dart

--- lib/widget/expenses_list/expense_item.dart
+++ lib/widget/expenses_list/expense_item.dart
@@ -18,7 +18,7 @@ class ExpenseItem extends StatelessWidget {
             const SizedBox(height: 4),
             Row(
               children: [
-                Text('R${expense.amount.toStringAsFixed(2)}'),
+                Text('$${expense.amount.toStringAsFixed(2)}'),
                 const Spacer(),
                 Row(
                   children: [
